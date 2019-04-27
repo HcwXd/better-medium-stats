70ebe4bcf051a32c0b5730b5ad561357b0be3d29
HcwXd <e04dl47878@gmail.com>
2019-04-28 03:34:20 +0800

    Update version to v1.4.0

--- popup.js
+++ popup.js
@@ -3,24 +3,22 @@ fetch('https://medium.com/me/stats?format=json&limit=100000')
     return response.text();
   })
   .then(function(textRes) {
-    let JsonRes = textRes.split('</x>')[1];
-    let data = JSON.parse(JsonRes);
-    let storyRawData = data.payload.value;
-    let totalStories = storyRawData.length;
-    let storyData = {
+    const data = JSON.parse(textRes.split('</x>')[1]);
+    const storyRawData = data.payload.value;
+    const totalStories = storyRawData.length;
+    const storyData = {
       totalViews: getTotal(storyRawData, 'views'),
       totalReads: getTotal(storyRawData, 'reads'),
       totalClaps: getTotal(storyRawData, 'claps'),
       totalUpvotes: getTotal(storyRawData, 'upvotes'),
       totalStories,
     };
-    render(storyData);
+    renderStoryData(storyData);
   })
-  .catch(function() {
-    document.querySelector('.loader').style.display = 'none';
-    document.querySelector(
-      '.container'
-    ).innerHTML = `<div class="label">Please log in to your Medium account :)<div>`;
+  .catch(function(err) {
+    const errorMsg = `<div class="label">Please log in to your Medium account :)<div>`;
+    document.querySelector('#table_container').innerHTML = errorMsg;
+    console.error(err);
   });
 
 function getTotal(arr, type) {
@@ -29,8 +27,8 @@ function getTotal(arr, type) {
   }, 0);
 }
 
-function render({ totalViews, totalReads, totalClaps, totalUpvotes, totalStories }) {
-  document.querySelector('.loader').style.display = 'none';
+function renderStoryData({ totalViews, totalReads, totalClaps, totalUpvotes, totalStories }) {
+  document.querySelector('#table_loader').style.display = 'none';
   const html = `
   <table>
       <thead>
@@ -60,6 +58,132 @@ function render({ totalViews, totalReads, totalClaps, totalUpvotes, totalStories
       </tbody>
     <table/>
   `;
-
   document.querySelector('.container').innerHTML = html;
+}
+
+let last30DaysStats = createLast30DaysObject();
+
+fetchNextNoti({ to: -1 });
+
+function createLast30DaysObject() {
+  let obj = {};
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = today.getMonth();
+  const date = today.getDate();
+  for (let i = 0; i <= 30; i++) {
+    const day = new Date(year, month - 1, date + i);
+    const key = day.getFullYear() * 10000 + (day.getMonth() + 1) * 100 + day.getDate();
+
+    obj[key] = {
+      follow: { count: 0, followers: [] },
+      highlight: { count: 0, posts: [] },
+      clap: { count: 0, posts: [] },
+    };
+  }
+  return obj;
+}
+
+function fetchNextNoti({ to }) {
+  const fetchUrl =
+    to === -1
+      ? 'https://medium.com/_/api/activity?limit=10000'
+      : `https://medium.com/_/api/activity?limit=${10000}&to=${to}`;
+
+  const isRollup = (type) => type.slice(type.length - 6, type.length) === 'rollup';
+
+  fetch(fetchUrl)
+    .then(function(response) {
+      return response.text();
+    })
+    .then(function(textRes) {
+      const data = JSON.parse(textRes.split('</x>')[1]);
+      const { value: notiRawData, paging } = data.payload;
+
+      notiRawData.forEach((notiItem) => {
+        if (isRollup(notiItem.activityType)) {
+          notiItem.rollupItems.forEach((noti) => countSingleNoti(noti));
+        } else {
+          countSingleNoti(notiItem);
+        }
+      });
+
+      if (paging && paging.next) {
+        fetchNextNoti(paging.next);
+      } else {
+        render30DaysStats();
+      }
+    })
+    .catch(function(err) {
+      document.querySelector('#line_loader').style.display = 'none';
+      console.error(err);
+    });
+}
+
+const eventType = {
+  follow: 'users_following_you',
+  highlight: 'quote',
+  clap: 'post_recommended',
+};
+
+function countSingleNoti(noti) {
+  let date = new Date(noti.occurredAt);
+  let key = date.getFullYear() * 10000 + (date.getMonth() + 1) * 100 + date.getDate();
+  if (last30DaysStats[key] !== undefined) {
+    if (noti.activityType === eventType.follow) {
+      last30DaysStats[key].follow.count++;
+      last30DaysStats[key].follow.followers.push(noti.actorId);
+    } else if (noti.activityType === eventType.clap) {
+      last30DaysStats[key].clap.count++;
+      last30DaysStats[key].clap.posts.push(noti.postId);
+    } else if (noti.activityType === eventType.highlight) {
+      last30DaysStats[key].highlight.count++;
+      last30DaysStats[key].highlight.posts.push(noti.postId);
+    }
+  } else {
+    console.log(key);
+  }
+}
+
+function render30DaysStats() {
+  console.log(last30DaysStats);
+  const ctx = document.getElementById('myChart').getContext('2d');
+  document.querySelector('#line_loader').style.display = 'none';
+  const chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: Object.keys(last30DaysStats).map(
+        (key) => `${Math.floor((key % 10000) / 100)}/${key % 100}`
+      ),
+      datasets: [
+        {
+          label: 'Daily followers',
+          borderColor: '#6eb799',
+          data: [...Object.keys(last30DaysStats).map((key) => last30DaysStats[key].follow.count)],
+        },
+        {
+          label: 'Daily claps',
+          borderColor: '#b7746e',
+          data: [...Object.keys(last30DaysStats).map((key) => last30DaysStats[key].clap.count)],
+        },
+        {
+          label: 'Daily highlights',
+          borderColor: '#b76eb1',
+          data: [
+            ...Object.keys(last30DaysStats).map((key) => last30DaysStats[key].highlight.count),
+          ],
+        },
+      ],
+    },
+
+    options: {
+      elements: {
+        line: {
+          backgroundColor: 'rgba(0,0,0,0)',
+          pointBackgroundColor: 'rgba(0,0,0,0)',
+          tension: 0,
+        },
+      },
+    },
+  });
 }
